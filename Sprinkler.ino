f256a0e64c00d0f3c0a8e50132fd6879e892ce11
Nayana Saji <33960554+ca999@users.noreply.github.com>
2017-11-26 19:00:41 +0530

    Add files via upload

---
+++ Sprinkler.ino
@@ -0,0 +1,35 @@
+#include<SoftwareSerial.h>
+SoftwareSerial BT(0,1);
+int motor=3;
+int led=13;
+long a;
+void setup()
+{
+  Serial.begin(9600);
+  BT.begin(9600);
+  pinMode(3,OUTPUT);
+  pinMode(13,OUTPUT);
+  Serial.println("Devise Activated");
+  digitalWrite(3,0);
+  digitalWrite(13,0);
+}
+void loop()
+{
+  if(Serial.available())
+  {
+    a=(BT.read());
+    if(a=='m')
+    {
+      Serial.println("Sprinkler on");
+      digitalWrite(13,1);
+      digitalWrite(3,1);
+    }
+    if(a=='n')
+    {
+      digitalWrite(13,0);
+      digitalWrite(3,0);
+      Serial.println("Sprinkler off");
+    }
+  }
+}
+  
